c82725601f49490c199a02924d9b666353251475
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-10-04 23:01:33 +0700

    add datatable

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,6 +9,8 @@
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
         integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous">
     </script>
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css ">
     <title>RC HUMIC - DASHBOARD</title>
 </head>
 
@@ -17,6 +19,13 @@
         @yield('content')
     </div>
     <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            let table = new DataTable('#example');
+        });
+    </script>
 </body>
 
 </html>
