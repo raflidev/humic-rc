f6ffd4713ebbd34b1d166dabb642e7a4eea6d75a
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-06-17 14:48:25 +0700

    add collapse

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -58,141 +58,122 @@
             </div>
             @auth
                 @if (Auth::user()->status == true)
-                    @if (Auth::user()->role == 'superadmin')
-                        <div class="mt-6">
-                            <div class="text-sm uppercase font-semibold text-slate-500">Area Superadmin</div>
-                            <div class="space-y-3 mt-5">
-                                <a href='{{ route('user.index') }}'
-                                    class="flex space-x-3 {{ Route::is('user.index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-user hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">User</div>
-                                </a>
-                                <a href='{{ route('user.index_admin') }}'
-                                    class="flex space-x-3 {{ Route::is('user.index_admin') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-user hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Admin</div>
-                                </a>
-                                <a href="{{ route('target.index_admin') }}"
-                                    class="flex space-x-3 {{ Route::is('target.index_admin') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-bullseye hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Target</div>
-                                </a>
-                                </a>
-                                <a href="{{ route('research.create_index') }}"
-                                    class="flex space-x-3 {{ Route::is('research.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-database hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Penelitian</div>
-                                </a>
-                                <a href="{{ route('publikasi.create_index') }}"
-                                    class="flex space-x-3 {{ Route::is('publikasi.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-database hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Publikasi</div>
-                                </a>
-                                <a href="{{ route('hki.create_index') }}"
-                                    class="flex space-x-3 {{ Route::is('hki.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-database hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input HKI</div>
-                                </a>
-                                <a href="{{ route('pengabdian.create_index') }}"
-                                    class="flex space-x-3 {{ Route::is('pengabdian.create_index') || Route::is('pengabdian.create') || Route::is('pengabdian.edit') || Route::is('pengabdian.excel_import')  ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-people-carry hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Pengabdian Masyarakat</div>
-                                </a>
+                    <div class="mt-6">
+                        <div class="text-sm uppercase font-semibold text-slate-500">
+                            @if (Auth::user()->role == 'user')
+                                Area User
+                            @else
+                                Area Superadmin
+                            @endif
+                        </div>
+                        <div class="space-y-3 mt-5">
+                            @if (Auth::user()->role == 'superadmin')
+                            <a href='{{ route('user.index') }}'
+                                class="flex space-x-3 {{ Route::is('user.index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-user hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">User</div>
+                            </a>
+                            <a href='{{ route('user.index_admin') }}'
+                                class="flex space-x-3 {{ Route::is('user.index_admin') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-user hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Admin</div>
+                            </a>
+                            @endif
+                            <a href="{{ route('target.index_admin') }}"
+                                class="flex space-x-3 {{ Route::is('target.index_admin') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-bullseye hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Input Target</div>
+                            </a>
+                            </a>
+                            <a href="{{ route('research.create_index') }}"
+                                class="flex space-x-3 {{ Route::is('research.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-database hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Input Penelitian</div>
+                            </a>
+                            <a href="{{ route('publikasi.create_index') }}"
+                                class="flex space-x-3 {{ Route::is('publikasi.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-database hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Input Publikasi</div>
+                            </a>
+                            <a href="{{ route('hki.create_index') }}"
+                                class="flex space-x-3 {{ Route::is('hki.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-database hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Input HKI</div>
+                            </a>
+                            <a href="{{ route('pengabdian.create_index') }}"
+                                class="flex space-x-3 {{ Route::is('pengabdian.create_index') || Route::is('pengabdian.create') || Route::is('pengabdian.edit') || Route::is('pengabdian.excel_import')  ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-people-carry hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Input Pengabdian Masyarakat</div>
+                            </a>
+                            <a id="collape-kerjasama" class="block flex space-x-3 justify-between items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase text-left w-full">Input Kerja Sama</div>
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-chevron-down hover:text-white text-slate-400"></i>
+                                </div>
+                            </a>
+                            <div id="kerjasama" class="bg-gray-300 p-2 rounded-md space-y-3 hidden">
                                 <a href="{{ route('kerjasama.moa') }}"
                                     class="flex space-x-3 {{ Route::is('kerjasama.moa') || Route::is('kerjasama.create_moa') || Route::is('kerjasama.edit_moa') || Route::is('kerjasama.excel_import_moa') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
                                     <div class="w-5 text-center">
                                         <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
                                     </div>
-                                    <div class="text-sm font-semibold uppercase">Input Kerja Sama - MOA</div>
+                                    <div class="text-sm font-semibold uppercase">MOA</div>
                                 </a>
                                 <a href="{{ route('kerjasama.mou') }}"
                                     class="flex space-x-3 {{ Route::is('kerjasama.mou') || Route::is('kerjasama.create_mou') || Route::is('kerjasama.edit_mou') || Route::is('kerjasama.excel_import_mou') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
                                     <div class="w-5 text-center">
                                         <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
                                     </div>
-                                    <div class="text-sm font-semibold uppercase">Input Kerja Sama - MOU</div>
+                                    <div class="text-sm font-semibold uppercase">MOU</div>
                                 </a>
                                 <a href="{{ route('kerjasama.ai') }}"
                                     class="flex space-x-3 {{ Route::is('kerjasama.ai') || Route::is('kerjasama.create_ai') || Route::is('kerjasama.edit_ai') || Route::is('kerjasama.excel_import_ai') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
                                     <div class="w-5 text-center">
                                         <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
                                     </div>
-                                    <div class="text-sm font-semibold uppercase">Input Kerja Sama - AI</div>
-                                </a>
-                                <a href="{{ route('ntf.index') }}"
-                                    class="flex space-x-3 {{ Route::is('ntf.index') || Route::is('ntf.create') || Route::is('ntf.edit') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input NTF</div>
+                                    <div class="text-sm font-semibold uppercase">IA</div>
                                 </a>
                             </div>
+                            <a href="{{ route('ntf.index') }}"
+                                class="flex space-x-3 {{ Route::is('ntf.index') || Route::is('ntf.create') || Route::is('ntf.edit') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Input NTF</div>
+                            </a>
+                            <a href='{{route('user.profile')}}'
+                                class="flex space-x-3 items-center {{ Route::is('user.profile') ? 'bg-slate-500 text-white' : '' }} hover:bg-slate-500 hover:text-white rounded p-2">
+                                <div class="w-5 text-center">
+                                    <i class="fas fa-user hover:text-white text-slate-400"></i>
+                                </div>
+                                <div class="text-sm font-semibold uppercase">Profile</div>
+                            </a>
                         </div>
-                    @endif
-                    @if (Auth::user()->role == 'user')
-                        <div class="mt-6">
-                            <div class="text-sm uppercase font-semibold text-slate-500">Area User</div>
-                            <div class="space-y-3 mt-5">
-                                <a href="{{ route('research.create_index') }}"
-                                    class="flex space-x-3 {{ Route::is('research.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-database hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Penelitian</div>
-                                </a>
-                                <a href="{{ route('pengabdian.create_index') }}"
-                                    class="flex space-x-3 {{ Route::is('pengabdian.create_index') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-people-carry hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Pengabdian Masyarakat</div>
-                                </a>
-                                <a href="{{ route('kerjasama.moa') }}"
-                                    class="flex space-x-3 {{ Route::is('kerjasama.moa') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Kerja Sama - MOA</div>
-                                </a>
-                                <a href="{{ route('kerjasama.mou') }}"
-                                    class="flex space-x-3 {{ Route::is('kerjasama.mou') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Kerja Sama - MOU</div>
-                                </a>
-                                <a href="{{ route('kerjasama.ai') }}"
-                                    class="flex space-x-3 {{ Route::is('kerjasama.ai') ? 'bg-slate-500 text-white' : '' }} items-center hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-network-wired hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Input Kerja Sama - AI</div>
-                                </a>
-                                <a href='{{route('user.profile')}}'
-                                    class="flex space-x-3 items-center {{ Route::is('user.profile') ? 'bg-slate-500 text-white' : '' }} hover:bg-slate-500 hover:text-white rounded p-2">
-                                    <div class="w-5 text-center">
-                                        <i class="fas fa-user hover:text-white text-slate-400"></i>
-                                    </div>
-                                    <div class="text-sm font-semibold uppercase">Profile</div>
-                                </a>
-                            </div>
-                        </div>
-                    @endif
+                    </div>
                 @endif
             @endauth
         </div>
     </div>
+<script>
+    document.getElementById('collape-kerjasama').addEventListener('click', function() {
+        document.getElementById('kerjasama').classList.toggle('hidden');
+    });
+
+</script>
